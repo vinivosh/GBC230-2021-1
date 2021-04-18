f81ec62982ed8d6cc417e13e508d53f931770de2
vinivosh <vinicius7427@gmail.com>
2021-04-18 20:02:37 -0300

    Prática 6 — Começando

--- Prática-6/K-Means.py
+++ Prática-6/K-Means.py
@@ -9,6 +9,7 @@ import os
 import shutil
 
 #Funções
+
 def euclDist(obj1 = None, obj2 = None): #Calcula a distância euclidiana entre dois objetos. Presume-se que sejam dois iterators com valores que sejam floats ou que possam ser transformados em. Caso algum erro seja encontrado, valor -1.0 é retornado.
     if (obj1 == None or obj2 == None or obj1 == [] or obj2 == []):
         print('\nErro! Algum dos objetos é nulo ou vazio. Retornando valor de erro (-1.0)…\n')
@@ -26,10 +27,10 @@ def euclDist(obj1 = None, obj2 = None): #Calcula a distância euclidiana entre d
         print(f'Cálculo da distância entre o objeto {obj1} e o objeto {obj2} falhou. Retornando valor de erro (-1.0)…\n')
         return -1.0
 
-def rmDupes(list_=[]):  #Remove valores duplicados de listas
-    if(list_ == [] or list_ == None):
+def rmDupes(_list=[]):  #Remove valores duplicados de listas
+    if(_list == [] or _list == None):
         return []
-    return list(dict.fromkeys(list_))
+    return list(dict.fromkeys(_list))
 
 ### Variáveis globais
 
@@ -61,9 +62,9 @@ while True: #Obtendo o caractere separador
         print('\nFavor insira um separador de exatamente um caractere!\n')
 
 while True: #Obtendo as variáveis relevantes para o cálculo de distância
-    colDist = input('\nInsira as colunas que representam as variáveis a serem consideradas para o cálculo de distância, separadas por espaços e iniciando em um. Apenas aperte enter para usar todas as colunas. (Por exemplo, se as variáveis relevantes forem as da 1ª, 3ª e 6ª colunas, bastaria inserir "1 3 6", sem as aspas): ').strip().split()
+    colDist = input('\nInsira as colunas que representam as variáveis a serem consideradas para o cálculo de distância, separadas por espaços e iniciando em um. Apenas aperte enter para usar o padrão para o Iris Dataset. (Por exemplo, se as variáveis relevantes forem as da 1ª, 3ª e 4ª colunas, bastaria inserir "1 3 4", sem as aspas): ').strip().split()
     if (colDist ==  []):
-        colDist = list(range(len(next(csvFile)))) #Cria uma lista de 0 até o número de colunas - 1
+        colDist = [1, 2, 3, 4] #O padrão do Iris Dataset, todas as 5 colunas, exceto a última.
         file.seek(0)   #Voltando a leitura do arquivo para o começo…
         break
     try:
@@ -73,7 +74,7 @@ while True: #Obtendo as variáveis relevantes para o cálculo de distância
         print('\nErro: ' + str(e) + '\nInsira um conjunto de colunas válido!')
 
 while True: #Ignorar primeira linha?
-    answer = input('\nA primeira linha possui nome das variáveis? (S)im ou (N)ão? (Enter para selecionar a opção padrão, que é não): ').strip().upper()
+    answer = input('\nA primeira linha possui os nomes das variáveis? (S)im ou (N)ão? (Enter para selecionar a opção padrão, que é não): ').strip().upper()
     if (answer == ''):
         ignore1stLine = False
         break
@@ -105,15 +106,15 @@ while True: #Truncamento do arquivo
         print(f'Insira um número inteiro entre 1 e {lenCsvFile}!')
 file.seek(0)   
 
-print('\nCriando arquivo temporário temp.csv…')
-shutil.copy(filePath, './temp.csv') #Copiando o arquivo para um segundo arquivo auxiliar
-print('\nCriando arquivo Distâncias.csv que terá os resultados…')
-fileAux = open('./temp.csv', 'r', encoding='utf-8')
-csvFileAux = csv.reader(fileAux, delimiter=separator)
-fileDists = open('./Distâncias.csv', 'w', encoding='utf-8')
+# print('\nCriando arquivo temporário temp.csv…')
+# shutil.copy(filePath, './temp.csv') #Copiando o arquivo para um segundo arquivo auxiliar
+print('\nCriando arquivo ResultadosKMeans.csv que terá os resultados…')
+# fileAux = open('./temp.csv', 'r', encoding='utf-8')
+# csvFileAux = csv.reader(fileAux, delimiter=separator)
+fileDists = open('./ResultadosKMeans.csv', 'w', encoding='utf-8')
 
 file.seek(0)
-fileAux.seek(0)
+# fileAux.seek(0)
 
 print('\nCalculando todas as distâncias…')
 print('Isso pode demorar muito! N² distâncias serão calculadas, onde N é a quantidade de objetos na base de dados menos aqueles após o truncamento.\n')
@@ -130,29 +131,17 @@ for row in csvFile: #Caluclando as distâncias…
         for index in colDist:
             rowRelevant.append(row[index])
         j = 0
-        for row2 in csvFileAux:
-            if (ignore1stLine and j == 0):
-                pass
-            elif (j > truncateLine):
-                break
-            else:
-                rowRelevant2 = []
-                for index in colDist:
-                    rowRelevant2.append(row2[index])
-                # print(euclDist(rowRelevant, rowRelevant2))
-                lineToWrite += f'{euclDist(rowRelevant, rowRelevant2)}, '
-            j += 1
     fileDists.write(f'{lineToWrite[:-2]}\n')
     # fileDists.write('\n')
-    fileAux.seek(0) #Voltando a leitura do arquivo auxiliar para o início
+    # fileAux.seek(0) #Voltando a leitura do arquivo auxiliar para o início
     i += 1
 
-print('\nConcluído, finalmente! Os resultados se encontram no arquivo Distâncias.csv, localizado na mesma pasta de onde este script foi rodado.')
+print('\nConcluído, finalmente! Os resultados se encontram no arquivo ResultadosKMeans.csv, localizado na mesma pasta de onde este script foi rodado.')
 
 file.close()
-fileAux.close()
-if os.path.exists('./temp.csv'):
-    print('\nApagando arquivo temporário…')
-    os.remove('./temp.csv')
+# fileAux.close()
+# if os.path.exists('./temp.csv'):
+#     print('\nApagando arquivo temporário…')
+#     os.remove('./temp.csv')
 
 print('\nFinalizando programa…\n\n')
